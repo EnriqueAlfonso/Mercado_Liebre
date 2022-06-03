a608db9a5b31fb7018f06e713c46b86321deff9f
Lema, E. A <enrique.alfonso.lema@gmail.com>
2022-06-03 09:50:14 -0300

    Version: 1.0

---
+++ app.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const path = require("path");
+const path_public = path.resolve(__dirname, "./public");
+
+app.listen(3000, () => console.log("Port: 3000"));
+
+app.use(express.static(path_public));
+
+app.get("/", (req, res) => {
+    let path_home = path.resolve(__dirname, "./views/home.html");
+
+    res.sendFile(path_home);
+});
+
+app.get("/register", (req, res) => {
+    let path_register = path.resolve(__dirname, "./views/register.html");
+
+    res.sendFile(path_register);
+});
